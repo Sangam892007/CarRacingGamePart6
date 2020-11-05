76660007d11d434d16d0afee09e670f582e09b44
Sangam892007 <63998016+Sangam892007@users.noreply.github.com>
2020-11-05 21:10:42 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,57 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+    this.reset = createButton('Reset');
+    this.a = createElement("h3");
+    this.b = createElement("h3");
+    this.c = createElement("h3");
+    this.d = createElement("h3");
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.reset.position(displayWidth-100,20);
+
+    this.a.position(50,0);
+    this.b.position(50,20);
+    this.c.position(50,40);
+    this.d.position(50,60);
+    this.a.html(player.rank);
+    this.b.html(player.rank);
+    this.c.html(player.rank);
+    this.d.html(player.rank);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
+      player.UpdateRank(0);
+    });
+
+  }
+}
